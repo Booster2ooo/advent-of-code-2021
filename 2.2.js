33a900d0e57f8c4e0d74afda6dcffb758a9463cb
Booster2ooo <the._.truth@hotmail.fr>
2021-12-08 16:51:08 +0100

    Create 2.2.js

---
+++ 2.2.js
@@ -0,0 +1,9 @@
+const data = input.reduce((acc, val) => {
+    let [move, amount] = val.split(' ');
+    amount = parseInt(amount, 10);
+    if (move === 'forward') { acc.h += amount; acc.d += acc.a * amount; }
+    if (move === 'down') { acc.a += amount }
+    if (move === 'up') { acc.a -= amount }
+	return acc;
+}, { h: 0, d:0, a:0 });
+data.h * data.d;
