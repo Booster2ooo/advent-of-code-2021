5453e352c7511447f673543134d3cc6223ad5ef5
Booster2ooo <the._.truth@hotmail.fr>
2021-12-08 17:32:05 +0100

    Create 3.1.js

---
+++ 3.1.js
@@ -0,0 +1,12 @@
+const γ = input.reduce((acc, val) => {
+    Array.from(val).forEach((bit, i) => {
+	    acc[i] = acc[i] || {};
+	    acc[i][bit] = acc[i][bit] || 0;
+	    acc[i][bit]++;
+	});
+    return acc;
+}, [])
+.map(counter => Object.keys(counter).reduce((i,j) => counter[i]>=counter[j] ? i : j))
+.reduce((acc, val) => acc + val, '')
+const ε = Array.from(γ).reduce((acc, i) => acc + (1 - parseInt(i, 10)), '');
+parseInt(γ, 2) * parseInt(ε, 2);
