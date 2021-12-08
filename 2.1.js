32c535128ecdc7966bab3ffe0ab35b27714ebce6
Booster2ooo <the._.truth@hotmail.fr>
2021-12-08 16:47:18 +0100

    Update 2.1.js

--- 2.1.js
+++ 2.1.js
@@ -2,8 +2,8 @@ const data = input.reduce((acc, val) => {
     let [move, amount] = val.split(' ');
     amount = parseInt(amount, 10);
     if (move === 'forward') { acc.h += amount }
-    if (move === 'down') { acc.d -= amount }
-    if (move === 'up') { acc.d += amount }
+    if (move === 'down') { acc.d += amount }
+    if (move === 'up') { acc.d -= amount }
     return acc;
 }, { h: 0, d: 0 });
 Math.abs(data.h * data.d);
